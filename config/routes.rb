c6060242bd322515124556335fb32d221e28d488
thaddavis <thaddadavis@gmail.com>
2016-07-04 12:11:45 -0400

    new, show ,create article implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'welcome/home', to: 'welcome#home'
   get 'welcome/about', to: 'welcome#about'
 
+  resources :articles
+
   # You can have the root of your site routed with "root"
   root 'welcome#home'
 
