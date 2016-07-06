013b2c9006721b03fc612884f5010d7e9d5146c2
thaddavis <thaddadavis@gmail.com>
2016-07-06 15:53:26 -0400

    Admin functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user && !current_user.admin?
         flash[:danger] = "Article was successfully deleted"
         redirect_to root_path
       end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 1)
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user.destroy
+    flash[:danger] = "User and all articles have been deleted"
+    redirect_to users_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
@@ -52,8 +59,15 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if !logged_in && current_user != @user
+      if current_user != @user && !current_user.admin?
         flash[:danger] = "Access Denied"
+        redirect_to root_path
+      end
+    end
+
+    def require_admin
+      if logged_in? && !current_user.admin?
+        flash[:danger] = "Only admin user can perform this action"
         redirect_to root_path
       end
     end
