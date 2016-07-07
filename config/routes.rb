33321ffd5751a233926386c1461a59fae28161bd
thaddavis <thaddadavis@gmail.com>
2016-07-07 12:43:28 -0400

    Add Categories and Tests

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
+  resources :categories, except: [:destroy]
+
   # You can have the root of your site routed with "root"
   root 'welcome#home'
 
